13d0d3f7a1ca2c0ad7044ac0c08fd74c2b19028c
Ghaida Bouchaala <ghaidabouchaala@Ghaidas-MacBook-Pro.local>
2022-10-13 08:22:17 +0100

    third principle & fixes

---
+++ LSP/src/com/directi/training/lsp/exercise_refactored/Pool.java
@@ -0,0 +1,33 @@
+package com.directi.training.lsp.exercise;
+
+public class Pool
+{
+    public void run()
+    {
+        Duck donaldDuck = new Duck();
+        ElectronicDuck electricDuck = new ElectronicDuck();
+        electricDuck.turnOn(); // turning on the electric duck.
+        quack(donaldDuck, electricDuck);
+        swim(donaldDuck, electricDuck);
+    }
+
+    private void quack(Duck... ducks)
+    {
+        for (Duck duck : ducks) {
+            duck.quack();
+        }
+    }
+
+    private void swim(Duck... ducks)
+    {
+        for (Duck duck : ducks) {
+            duck.swim();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Pool pool = new Pool();
+        pool.run();
+    }
+}
--- OCP/src/com/directi/training/ocp/exercise_refactored/ResourceAllocator.java
+++ OCP/src/com/directi/training/ocp/exercise_refactored/ResourceAllocator.java
@@ -7,35 +7,24 @@ public class ResourceAllocator
     public int allocate(Resource resource)
     {
         int resourceId;
-        switch (resource) {
-        case TIME_SLOT:
-            resourceId = findFreeSlot();
-            markSlotBusy(resourceId);
-            break;
-        case SPACE_SLOT:
-            resourceId = findFreeSlot();
-            markSlotBusy(resourceId);
-            break;
-        default:
-            System.out.println("ERROR: Attempted to allocate invalid resource");
-            resourceId = INVALID_RESOURCE_ID;
-            break;
+        if (resource)
+        {
+             resourceId = findFreeSlot();
+             markSlotBusy(resourceId);
+        } else {
+              System.out.println("ERROR: Attempted to allocate invalid resource");
+              resourceId = INVALID_RESOURCE_ID;
+
         }
         return resourceId;
     }
 
     public void free(Resource resource, int resourceId)
     {
-            switch (resource) {
-            case TIME_SLOT:
-                markSlotFree(resourceId);
-                break;
-            case SPACE_SLOT:
-                markSlotFree(resourceId);
-                break;
-            default:
-                System.out.println("ERROR: attempted to free invalid resource");
-                break;
-            }
+         if (resource)
+             markSlotFree(resourceId);
+         else
+             System.out.println("ERROR: attempted to free invalid resource");
+
     }
 }
